ac354cf06272fcf9ddb2ce1726b8d3e84bcba36f
Administrator <897540438@qq.com>
2018-08-20 17:05:08 +0800

    初步理顺vue.js工作流程，上传页面template(模板)

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import UserLogin from '@/components/UserLogin'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+    	path:'/UserLogin',
+    	name:'UserLogin',
+    	component:UserLogin	
     }
   ]
 })
